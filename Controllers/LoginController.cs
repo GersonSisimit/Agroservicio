55fb1485ee9f44b2b57a6e737082f6534d77b0ee
Gerson Sisimit <infotec@disdelsa.com>
2024-10-09 17:33:37 -0600

    Login con catpcha

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -9,6 +9,7 @@ using System.Text;
 using Newtonsoft;
 using Agroservicio.Migrations;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 
 namespace Agroservicio.Controllers
@@ -30,64 +31,11 @@ namespace Agroservicio.Controllers
             return View();
         }
 
-        [HttpPost]
-        public IActionResult Login2(Usuario value)
-        {
-            try
-            {
-                var UsuarioNombre = _contextDB.Usuario.
-                Where(x => x.Nombre == value.Nombre)
-                .FirstOrDefault();
-
-                var Usuario = _contextDB.Usuario.
-                Where(x => x.Nombre == value.Nombre && x.Contraseña == EncriptarString(value.Contraseña))
-                .FirstOrDefault();
-
-                if (Usuario != null)
-                {
-
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                {
-                    TempData["Resultado"] = "Error";
-                    if (UsuarioNombre != null)
-                    {
-                        TempData["Mensaje"] = "Contraseña incorrecta";
-                    }
-                    else
-                    {
-                        TempData["Mensaje"] = "Usuario no encontrado";
-                    }
-                }
-            }
-            catch (Exception Error)
-            {
-                TempData["Resultado"] = "Error";
-                TempData["Mensaje"] = Error.Message;
-            }
-            return View();
-
-
-
-
-
-
-            //HttpContext.Session.SetString("Message", "Hello MVC!");
-
-
-
-            //string message = HttpContext.Session.GetString("Message");
-
-
-        }
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Index", "Home"); // Redirigir a la página principal
         }
-
-
 
         [HttpPost]
         public async Task<IActionResult> Login(Usuario value)
@@ -97,41 +45,53 @@ namespace Agroservicio.Controllers
                 return View(); // Mantener errores de validación
             }
 
+            // Obtener el token de reCAPTCHA del formulario
+            var captchaResponse = Request.Form["g-recaptcha-response"];
+            var secretKey = "6LffBl0qAAAAAM0srWEfYgZzsOppCbmCwmX7WfxY";
+
+            // Validar el token de reCAPTCHA con la API de Google
+            var httpClient = new HttpClient();
+            var response = await httpClient.PostAsync($"https://www.google.com/recaptcha/api/siteverify?secret={secretKey}&response={captchaResponse}", null);
+            var jsonResult = await response.Content.ReadAsStringAsync();
+            dynamic result = JObject.Parse(jsonResult);
+
+            // Verificar si el captcha fue exitoso
+            if (result.success != true)
+            {
+                TempData["Resultado"] = "Error";
+                TempData["Mensaje"] = "Verificación de CAPTCHA fallida.";
+                return View(); // Mostrar el formulario de inicio de sesión con el mensaje de error
+            }
 
             try
             {
-                var UsuarioNombre = _contextDB.Usuario.
-                Where(x => x.Nombre == value.Nombre)
-                .FirstOrDefault();
+                var UsuarioNombre = _contextDB.Usuario
+                    .Where(x => x.Nombre == value.Nombre)
+                    .FirstOrDefault();
 
-                var Usuario = _contextDB.Usuario.
-                Where(x => x.Nombre == value.Nombre && x.Contraseña == EncriptarString(value.Contraseña))
-                .FirstOrDefault();
+                var Usuario = _contextDB.Usuario
+                    .Where(x => x.Nombre == value.Nombre && x.Contraseña == EncriptarString(value.Contraseña))
+                    .FirstOrDefault();
 
                 if (Usuario != null)
                 {
-
                     var claims = new List<Claim>
                     {
                         new Claim(ClaimTypes.Name, value.Nombre),
                         new Claim("IdUsuario", value.Id.ToString()),
-                        // Agregar claims adicionales según sea necesario
                     };
 
                     var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
                     var authProperties = new AuthenticationProperties
                     {
-                        IsPersistent = true // Opcional: Permitir inicios de sesión persistentes (cookies en las sesiones del navegador)
+                        IsPersistent = true // Mantener la sesión iniciada
                     };
 
                     await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
                         new ClaimsPrincipal(claimsIdentity), authProperties);
 
-                    //Guardando la informacion del usuario 
-
                     var json = JsonConvert.SerializeObject(Usuario);
                     HttpContext.Session.SetString("Usuario", json);
-
 
                     return RedirectToAction("Index", "Home");
                 }
@@ -153,9 +113,9 @@ namespace Agroservicio.Controllers
                 TempData["Resultado"] = "Error";
                 TempData["Mensaje"] = Error.Message;
             }
-            return View(); // Mostrar el formulario de inicio de sesión con un mensaje de error
-        }
 
+            return View(); // Mostrar el formulario de inicio de sesión con el mensaje de error
+        }
 
         [HttpPost]
         public IActionResult Registrarse(Usuario value)
